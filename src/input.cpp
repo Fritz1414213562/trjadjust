111b7d7527b545c9f31991f1ea8f244982cc788d
Fritz NAGAE <fritz1414213562@gmail.com>
2025-09-11 00:58:17 +0900

    feat: add a flag for moving molecules at a center

--- src/input.cpp
+++ src/input.cpp
@@ -12,6 +12,7 @@ Input::Input(int argc, char* argv[])
 		("output, o", boost_po::value<std::string>(), "path to an output trajectory")
 		("delta, d",  boost_po::value<float>()->default_value(0.3), "time increment")
 		("step, s",   boost_po::value<std::size_t>()->default_value(10000), "interval for recording")
+		("center, c", "Move molecules specified in topology file onto a center")
 		("help, h", "produce help message");
 	boost_po::variables_map vm;
 	try
@@ -29,9 +30,10 @@ Input::Input(int argc, char* argv[])
 		std::cerr << desc << std::endl;
 		std::exit(1);
 	}
-	this->input_traj_name_  = vm["traj"].as<std::string>();
-	this->topology_name_    = vm["topo"].as<std::string>();
-	this->output_traj_name_ = vm["output"].as<std::string>();
-	this->delta_t_          = vm["delta"].as<float>();
-	this->step_interval_    = vm["step"].as<std::size_t>();
+	this->input_traj_name_        = vm["traj"].as<std::string>();
+	this->topology_name_          = vm["topo"].as<std::string>();
+	this->output_traj_name_       = vm["output"].as<std::string>();
+	this->delta_t_                = vm["delta"].as<float>();
+	this->step_interval_          = vm["step"].as<std::size_t>();
+	this->is_moved_to_center_     = vm.count("center");
 }
--- src/input.hpp
+++ src/input.hpp
@@ -4,22 +4,25 @@
 #include <string>
 #include <iostream>
 #include <vector>
+#include <array>
 
 class Input
 {
 	public:
 		Input(int argc, char* argv[]);
-		std::string input_traj_name() const { return input_traj_name_; }
-		std::string output_traj_name() const { return output_traj_name_; }
-		std::string topology_name()   const { return topology_name_; }
-		float       delta_t()         const { return delta_t_; }
-		std::size_t step_interval()   const { return step_interval_; }
+		std::string input_traj_name()    const { return input_traj_name_; }
+		std::string output_traj_name()   const { return output_traj_name_; }
+		std::string topology_name()      const { return topology_name_; }
+		float       delta_t()            const { return delta_t_; }
+		std::size_t step_interval()      const { return step_interval_; }
+		bool        is_moved_to_center() const { return is_moved_to_center_; }
 	private:
 		std::string input_traj_name_;
 		std::string output_traj_name_;
 		std::string topology_name_;
 		float       delta_t_;
 		std::size_t step_interval_;
+		bool        is_moved_to_center_;
 };
 
 #endif // TRJADJUST_INPUT_HPP
